0bd9df51d3d82c63ca822eb44c4df16bf188b339
André <andrecjuniorr@gmai.com>
2026-06-16 12:40:53 -0300

    Métodos que retornam objetoss

--- src/Principal.java
+++ src/Principal.java
@@ -4,15 +4,15 @@ public class Principal {
         Paciente paciente = new Paciente();
         paciente.altura = 1.85;
         paciente.peso = 155.0;
-        /*agora o imc não é mais um double é o tipo peso
-        IndiceMassaCorporal . Para retornar um objeto basta vc criar
-        a classe que vai representar ela nesse caso o IndiceMassaCorporal
-        Basta criar a classe para representar esse objeto no caso o
-         IndiceMassaCorporal com os atributos vc tbm pode ter métodos aqui
-         em outra aula isso será abordado, e nesse caso está servindo como uma
-         estrutura de dados é um objeto para carregar dados não sendo sempre o
-         ideal, se vc puder adcionar metódos aqui vc deixa sua classe mais rica
-         */
+/*agora o imc não é mais um double é o tipo peso
+IndiceMassaCorporal . Para retornar um objeto basta vc criar
+a classe que vai representar ela nesse caso o IndiceMassaCorporal
+Basta criar a classe para representar esse objeto no caso o
+ IndiceMassaCorporal com os atributos vc tbm pode ter métodos aqui
+ em outra aula isso será abordado, e nesse caso está servindo como uma
+ estrutura de dados é um objeto para carregar dados não sendo sempre o
+ ideal, se vc puder adcionar metódos aqui vc deixa sua classe mais rica
+ */
         IndiceMassaCorporal imc = paciente.calcularIndiceMassaCorporal();
 
         if (imc.resultado >= 30) {
